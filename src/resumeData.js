fd3dff35b5804092d349f64cb5a1db116f09736f
Vince <user@Users-Air.fios-router.home>
2021-01-29 23:16:36 -0500

    added resumeData and code

---
+++ src/resumeData.js
@@ -0,0 +1,81 @@
+let resumeData = {
+  "imagebaseurl": "https://ivy555.github.io/",
+  "name": "Vince Lanzilotti",
+  "role": "Software Developer",
+  "linkedinId": "vlanzilo",
+  "roleDescription": "As a previous educator, I believe my attention to detail, organization, and desire to learn   will allow me to advance as a software developer. By means of collaboration, we will build a successful product.",
+  "socialLinks":[
+    {
+      "name": "linkedin",
+      "url": "https://linkedin.com/in/vlanzilo",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/vlanzilo87",
+      "className": "fa fa-github"
+    }
+  ],
+  "aboutme": "Welcome to my portfolio! My name is Vince Lanzilotti and I'm beginning my career in software development. I've been a teacher for about 10 years when I decided it was time for a change. Although I'm relatively new to this field, I really enjoy it and look forward to growing. I've attached a couple links to some previous projects for you to check out.",
+  "address": "Philadelphia, Pennsylvania",
+  "website": "this portfolio",
+  "education": [
+    {
+      "UniversityName": "General Assembly",
+      "specialization": "Software Engineering",
+      "MonthOfPassing": "April",
+      "YearOfPassing": "2020",
+      "Achievements": "Certification"
+    },
+    {
+      "UniversityName": "Champlain College",
+      "specialization": "Education",
+      "MonthOfPassing": "August",
+      "YearOfPassing": "2009",
+      "Achievements": "Bachelors of Science"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "General Assembly",
+      "specialization": "Software Engineering Fellow",
+      "MonthOfLeaving": "April",
+      "YearOfLeaving": "2020"
+    },
+    {
+      "CompanyName": "CC Charter School",
+      "specialization": "Special Education Teacher",
+      "MonthOfLeaving": "January",
+      "YearOfLeaving": "2020"
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {"skillname": "HTML"},
+    {"skillname": "CSS"},
+    {"skillname": "JavaScript"},
+    {"skillname": "ReactJS"}
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    }
+  ]
+}

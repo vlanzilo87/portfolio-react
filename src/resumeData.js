b23493524843a17b831994fee6a7e98ba596aec0
Vince <user@Users-Air.fios-router.home>
2021-02-05 09:03:28 -0500

    added npm jquery and fontawesome

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,10 +1,15 @@
 let resumeData = {
-  "imagebaseurl": "https://ivy555.github.io/",
+  "imagebaseurl": "https://vlanzilo.imgur.com/all",
+
   "name": "Vince Lanzilotti",
+
   "role": "Software Developer",
+
   "linkedinId": "vlanzilo",
+
   "roleDescription": "As a previous educator, I believe my attention to detail, organization, and desire to learn   will allow me to advance as a software developer. By means of collaboration, we will build a successful product.",
-  "socialLinks":[
+
+  "socialLinks": [
     {
       "name": "linkedin",
       "url": "https://linkedin.com/in/vlanzilo",
@@ -16,9 +21,13 @@ let resumeData = {
       "className": "fa fa-github"
     }
   ],
+
   "aboutme": "Welcome to my portfolio! My name is Vince Lanzilotti and I'm beginning my career in software development. I've been a teacher for about 10 years when I decided it was time for a change. Although I'm relatively new to this field, I really enjoy it and look forward to growing. I've attached a couple links to some previous projects for you to check out.",
+
   "address": "Philadelphia, Pennsylvania",
+
   "website": "this portfolio",
+
   "education": [
     {
       "UniversityName": "General Assembly",
@@ -35,6 +44,7 @@ let resumeData = {
       "Achievements": "Bachelors of Science"
     }
   ],
+
   "work": [
     {
       "CompanyName": "General Assembly",
@@ -49,13 +59,16 @@ let resumeData = {
       "YearOfLeaving": "2020"
     }
   ],
+
   "skillsDescription": "Your skills here",
+
   "skills": [
     {"skillname": "HTML"},
     {"skillname": "CSS"},
     {"skillname": "JavaScript"},
     {"skillname": "ReactJS"}
   ],
+
   "portfolio": [
     {
       "name": "project1",
@@ -79,3 +92,5 @@ let resumeData = {
     }
   ]
 }
+
+export default resumeData

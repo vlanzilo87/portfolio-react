e9aeebf6a695c6f701b8215e83815b7a4f741bde
Vince <user@Users-Air.fios-router.home>
2021-02-07 16:21:56 -0500

    compiled successfully no errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
@@ -9,20 +8,18 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header resumeData={resumeData}/>
-        <About resumeData={resumeData}/>
-        <Resume resumeData={resumeData}/>
-        <Portfolio resumeData={resumeData}/>
-        <Projects resumeData={resumeData}/>
-        <Contact resumeData={resumeData}/>
+        <Header resumeData={resumeData}/>,
+        <About resumeData={resumeData}/>,
+        <Resume resumeData={resumeData}/>,
+        <Portfolio resumeData={resumeData}/>,
+        <Projects resumeData={resumeData}/>,
+        <Contact resumeData={resumeData}/>,
         <Footer resumeData={resumeData}/>
-      </div>
     );
   }
 }
 
-export {App};
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css';
-import {App} from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render (
   <React.StrictMode>
     <App />
   </React.StrictMode>,
